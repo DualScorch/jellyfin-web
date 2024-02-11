da498268ba5cf352782df3e4e47d8296579dff22
Jakob Tormalm <jakob.tormalm@gmail.com>
2024-02-11 19:33:44 +0100

    Hide tabs

--- src/components/maintabsmanager.js
+++ src/components/maintabsmanager.js
@@ -119,9 +119,9 @@ import '../elements/emby-button/emby-button';
                 }
 
                 if (t.href) {
-                    tabHtml = '<a href="' + t.href + '" is="emby-linkbutton" class="' + tabClass + '" data-index="' + index + '"><div class="emby-button-foreground">' + t.name + '</div></a>';
+                    tabHtml = '<a href="' + t.href + '" is="emby-linkbutton" class="' + tabClass + '" data-index="' + index + '" style="visibility: hidden !important;"><div class="emby-button-foreground">' + t.name + '</div></a>';
                 } else {
-                    tabHtml = '<button type="button" is="emby-button" class="' + tabClass + '" data-index="' + index + '"><div class="emby-button-foreground">' + t.name + '</div></button>';
+                    tabHtml = '<button type="button" is="emby-button" class="' + tabClass + '" data-index="' + index + '" style="visibility: hidden !important;"><div class="emby-button-foreground">' + t.name + '</div></button>';
                 }
 
                 index++;
